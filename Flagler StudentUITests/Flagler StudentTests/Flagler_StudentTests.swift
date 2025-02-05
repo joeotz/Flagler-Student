1787c499732ddaa07ab25d35829da74d5df91190
Joe Otzelberger <joeotz@protonmail.com>
2025-02-05 14:42:50 -0500

    Initial Commit

---
+++ Flagler StudentUITests/Flagler StudentTests/Flagler_StudentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Flagler_StudentTests.swift
+//  Flagler StudentTests
+//
+//  Created by Joseph Otzelberger on 1/27/25.
+//
+
+import Testing
+@testable import Flagler_Student
+
+struct Flagler_StudentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
